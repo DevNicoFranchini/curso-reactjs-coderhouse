6e2469bdb99649f69363a9d70f745d28cac64a0f
Nicolas Franchini <devnicolasfranchini@gmail.com>
2022-07-09 19:18:38 -0300

    agregado de productos

--- las-flores-pasteleria/src/helpers/getFetch.js
+++ las-flores-pasteleria/src/helpers/getFetch.js
@@ -1,17 +1,49 @@
 const productos = [
   {
     id: 1,
-    tipo: "Tortas",
+    tipo: "tortas",
     img: "./../imgs/tortas/coco.jpg",
     nombre: "Torta de coco",
+    descripcion:
+      "A lo seguro. Torta de ricota bien fresquita con decoración de dulce de leche. ¡Ideal para los mates!",
     cantidad: 1,
     precio: "$1000",
   },
   {
     id: 2,
-    tipo: "Tortas",
+    tipo: "tortas",
     img: "./../imgs/tortas/frutilla.jpg",
     nombre: "Torta de frutilla",
+    descripcion:
+      "Riquisima torta de frutilla con base de bizcochuelo y decoración de merengue a elección.",
+    cantidad: 2,
+    precio: "$2000",
+  },
+  {
+    id: 3,
+    tipo: "tortas",
+    img: "./../imgs/tortas/lemon.jpg",
+    nombre: "Torta de limón",
+    descripcion:
+      "Clásica torta lemon pie con base de bizcochuelo y decoración de copitos merengue.",
+    cantidad: 4,
+    precio: "$3000",
+  },
+  {
+    id: 4,
+    tipo: "galletas",
+    img: "./../imgs/galletas/avena.jpg",
+    nombre: "Galleta de Avena",
+    descripcion: "Riquisima galleta de avena.",
+    cantidad: 1,
+    precio: "$1000",
+  },
+  {
+    id: 5,
+    tipo: "galletas",
+    img: "./../imgs/galletas/chocolate.jpg",
+    nombre: "Alfajor de Chocolate",
+    descripcion: "Clásico alfajor de chocolate.",
     cantidad: 2,
     precio: "$2000",
   },
@@ -25,5 +57,5 @@ export const gFetch = new Promise((res, rej) => {
     } else {
       rej("Error");
     }
-  }, 1000);
+  }, 2000);
 });
