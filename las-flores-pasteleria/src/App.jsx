3b0c9b3aeed022f1320891d48b4489ae0e872790
Nicolas Franchini <devnicolasfranchini@gmail.com>
2022-07-18 18:28:29 -0300

    Aplicacion de context en app

--- las-flores-pasteleria/src/App.jsx
+++ las-flores-pasteleria/src/App.jsx
@@ -4,25 +4,27 @@ import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/container/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/container/ItemDetailContainer/ItemDetailContainer";
 import Cart from "./components/container/Cart/Cart";
+import CartProvider from "./context/CartContext";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <NavBar />
-        <Routes>
-          <Route index path="/" element={<ItemListContainer />} />
-          <Route index path="/tipo/:tipo" element={<ItemListContainer />} />
-          <Route path="/detail/:id" element={<ItemDetailContainer />} />
-          <Route path="/cart" element={<Cart />} />
-
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <div>
+          <NavBar />
+          <Routes>
+            <Route index path="/" element={<ItemListContainer />} />
+            <Route index path="/tipo/:tipo" element={<ItemListContainer />} />
+            <Route path="/detail/:id" element={<ItemDetailContainer />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
