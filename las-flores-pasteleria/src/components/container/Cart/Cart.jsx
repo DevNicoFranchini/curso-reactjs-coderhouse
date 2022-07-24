091365703626a370751a0d2c05439ac21bfa9054
Nicolas Franchini <devnicolasfranchini@gmail.com>
2022-07-24 20:48:59 -0300

    se mapea el cart y funciones finales

--- las-flores-pasteleria/src/components/container/Cart/Cart.jsx
+++ las-flores-pasteleria/src/components/container/Cart/Cart.jsx
@@ -1,5 +1,40 @@
+import { useCartContext } from "../../../context/CartContext";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const { cart, emptyCart, precioTotal, eliminarProducto } = useCartContext();
+
+  return (
+    <div>
+      <h1>Carrito</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <h2>Producto</h2>
+          <ul>
+            {cart.map((product) => (
+              <>
+                <li key={product.id}>
+                  <img src={product.img} className="w-25" />
+                  <span> - Nombre: {product.nombre}</span>
+                  <span> - Precio: ${product.precio}</span>
+                  <span> - Cantidad: {product.cantidad}</span>
+                  <button onClick={() => eliminarProducto(product.id)}>
+                    {" "}
+                    X{" "}
+                  </button>
+                </li>
+              </>
+            ))}
+          </ul>
+        </div>
+        <div className="w-25">
+          <button onClick={emptyCart}>Vaciar Carrito</button>
+        </div>
+        <div className="w-25">
+          {precioTotal() !== 0 && `Precio Total: $${precioTotal()}`}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
