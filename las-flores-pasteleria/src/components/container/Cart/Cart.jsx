2d6f9308297b56980391e1f5de2e2cdd68180fc4
Nicolas Franchini <devnicolasfranchini@gmail.com>
2022-08-13 09:43:08 -0300

    estilos cart

--- las-flores-pasteleria/src/components/container/Cart/Cart.jsx
+++ las-flores-pasteleria/src/components/container/Cart/Cart.jsx
@@ -1,42 +1,76 @@
 import { useCartContext } from "../../../context/CartContext";
 import { Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { getFirestore, addDoc, collection } from "firebase/firestore";
 
 import ItemCart from "../../Item/ItemCart";
 
 const Cart = () => {
   const { cart, emptyCart, precioTotal } = useCartContext();
 
+  const generateOrder = () => {
+    const order = {};
+    (order.buyer = {
+      name: "Nombre",
+      phone: "11111",
+      email: "nombre@gmail.com",
+    }),
+      (order.items = cart.map((prod) => {
+        const id = prod.id;
+        const title = prod.nombre;
+        const cantidad = prod.cantidad;
+
+        return { id, title, cantidad };
+      }));
+    order.total = precioTotal();
+
+    const db = getFirestore();
+    const queryInsertCollection = collection(db, "orders");
+    addDoc(queryInsertCollection, order)
+      .then((resp) => console.log(resp))
+      .catch((err) => console.log(err))
+      .finally(() => emptyCart());
+  };
+
   return (
     <Container>
       <div>
-        <div className="d-flex align-items-center">
-          <h3>{cart.length == 0 && `Carrito`}</h3>
-          <div className="ms-5">
-            {cart.length === 0 ? (
-              ""
-            ) : (
-              <Button variant="danger" onClick={emptyCart}>
-                Vaciar Carrito
-              </Button>
-            )}
-          </div>
-        </div>
+        <div className="ms-5">
+          {cart.length === 0 ? (
+            <div className="d-flex flex-column align-items-center justify-content-center my-2">
+              <h3>
+                {cart.length == 0 && `Tu carrito de compras se encuentra vacío`}
+              </h3>
 
-        <div className="row">
-          <div className="col-md-6">
-            {cart.map((product) => (
-              <ItemCart key={product.id} product={product} />
-            ))}
-          </div>
-          <div className="w-25">
-            {precioTotal() !== 0 && `Precio Total: $${precioTotal()}`}
-          </div>
-          <Link to="/">
-            <div className="w-100">
-              {cart.length == 0 && `UPS! Carrito vacío. Volver al inicio`}
+              <Link to="/">
+                <h3>
+                  {cart.length == 0 && `Volver al inicio y empezar a comprar`}
+                </h3>
+              </Link>
             </div>
-          </Link>
+          ) : (
+            <Container className="d-flex my-4">
+              <div className="d-flex justify-content-evenly col-10">
+                <div>
+                  {cart.map((product) => (
+                    <ItemCart key={product.id} product={product} />
+                  ))}
+                </div>
+              </div>
+
+              <div className="d-flex flex-column gap-2 col-2">
+                <div className="w-100">
+                  {precioTotal() !== 0 && `Precio final: $${precioTotal()}`}
+                </div>
+                <Button variant="danger" size="sm" onClick={emptyCart}>
+                  Vaciar Carrito
+                </Button>
+                <Button variant="primary" size="sm" onClick={generateOrder}>
+                  Generar Orden
+                </Button>
+              </div>
+            </Container>
+          )}
         </div>
       </div>
     </Container>
